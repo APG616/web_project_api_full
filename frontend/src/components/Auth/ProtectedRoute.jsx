aeb52330730d008cf80cfecb0c7e397b54b0a370
DanielPatino <andpague@protonmail.com>
2025-06-10 21:29:49 -0500

    Optimizar Api

--- frontend/src/components/Auth/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-// ProtectedRoute.jsx
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function ProtectedRoute({ isLoggedIn }) {
-  return isLoggedIn ? <Outlet /> : <Navigate to="/signin" replace />;
-}
